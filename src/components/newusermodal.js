7282aa1ff9208260d14245905da88d8c5e0afd62
Subash-G <subashg@mieupro.com>
2019-10-11 18:55:39 +0530

    newuser modal window for successful registraion

---
+++ src/components/newusermodal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { Button } from 'reactstrap';
+
+
+
+function newUserModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+    
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.headerTitle}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body style={{'max-height': 'calc(100vh - 200px)', 'overflow-y': 'auto'}}>
+        
+        <p>
+          {props.description}
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+
+export default newUserModal;
