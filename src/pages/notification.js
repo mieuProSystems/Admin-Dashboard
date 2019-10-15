e18ff05349b8d4ce933bb3f86473ab15d8b81370
Subash-G <subashg@mieupro.com>
2019-10-15 18:29:19 +0530

    Notification page created

---
+++ src/pages/notification.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import IPADDRESS from '../components/server_ip';
+class notification extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    componentDidMount(){
+        fetch(IPADDRESS+'/home/user/getFeedback')
+        .then(response=>response.json())
+        .then((resultData)=>{
+            console.log(resultData);
+        })
+    }
+    render() { 
+        return ( <h4>Notification Page! <i>(Not Yet Completed)</i></h4> );
+    }
+}
+ 
+export default notification ;
