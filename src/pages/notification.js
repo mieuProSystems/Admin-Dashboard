d9d7e4e1973e5459729ba1bbe045d22f83a158f1
Subash-G <subashg@mieupro.com>
2019-10-31 13:06:51 +0530

    moved notification fetch in homepage

--- src/pages/notification.js
+++ src/pages/notification.js
@@ -6,13 +6,6 @@ class notification extends Component {
         this.state = {  }
     }
 
-    componentDidMount(){
-        fetch(IPADDRESS+'/home/admin/getFeedback')
-        .then(response=>response.json())
-        .then((resultData)=>{
-            console.log(resultData);
-        })
-    }
     render() { 
         return ( <h4>Notification Page! <i>(Not Yet Completed)</i></h4> );
     }
